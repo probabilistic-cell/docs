36c34fd2a2565dab0440f6dc889baab4fb503aed
Wouter Saelens <wouter.saelens@gmail.com>
2021-08-03 11:04:29 +0200

    update cookbooks

---
+++ cookbook/regression/splines.py
@@ -0,0 +1,222 @@
+# %% [markdown]
+# # Splines
+
+# %%
+from IPython import get_ipython
+if get_ipython():
+    get_ipython().run_line_magic('load_ext', 'autoreload')
+    get_ipython().run_line_magic('autoreload', '2')
+
+import numpy as np
+import pandas as pd
+import torch
+
+import xarray as xr
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import collections
+
+import latenta as la
+la.logger.setLevel("INFO")
+
+# %% [markdown]
+# ## Random walks
+
+# %%
+cells = la.Dim(pd.Series(range(100), name = "cell").astype(str))
+genes = la.Dim(pd.Series(range(4), name = "gene").astype(str))
+
+# %%
+dist = la.distributions.RandomWalk(10, la.distributions.Normal())
+
+# %%
+dist.run_recursive()
+print(dist.value)
+print(dist.likelihood)
+
+# %%
+knots = la.Dim(range(10), "knot")
+
+# %%
+dist = la.distributions.RandomWalk(10, la.distributions.Normal(), definition = la.Definition([genes, knots]))
+
+# %%
+dist.run_recursive()
+print(dist.value)
+print(dist.likelihood)
+
+# %%
+knots = la.Dim(range(10), "knot")
+
+# %%
+dist = la.distributions.RandomWalk(10, la.distributions.Normal(), definition = la.Definition([knots, genes]))
+
+# %%
+dist.reset_recursive()
+dist.run_recursive()
+print(dist.value)
+print(dist.step.likelihood)
+print(dist.likelihood)
+print(dist.log_prob(dist.value))
+print(dist.log_prob(torch.zeros(dist.value.shape)))
+
+# %%
+dist = la.distributions.RandomWalk(10, la.distributions.Normal(definition = la.Definition([genes])))
+dist.plot()
+
+# %%
+dist.reset_recursive()
+dist.run_recursive()
+print(dist.value)
+print(dist.step.likelihood)
+print(dist.likelihood)
+print(dist.log_prob(dist.value))
+print(dist.log_prob(torch.zeros(dist.value.shape)))
+
+# %%
+genes = la.Dim(pd.Series(range(100), name = "gene").astype(str))
+
+# %%
+dist = la.distributions.RandomWalk(100, la.distributions.Normal(definition = la.Definition([genes])))
+dist.run_recursive()
+value = dist.value.cpu().numpy()
+for i in range(value.shape[0]):
+    sns.lineplot(x = np.arange(value.shape[1]), y = value[i], alpha = 0.3)
+
+# %% [markdown]
+# ## Generative model
+
+# %%
+n_cells = 50
+cell_ids = [str(i) for i in range(n_cells)]
+cells = la.Dim(pd.Series(cell_ids, name = "cell"))
+
+x = la.Fixed(pd.Series(np.random.uniform(0, 3, n_cells), index = cells.index), label = "x")
+
+
+# %%
+n_genes = 100
+n_knots = 10
+genes = la.Dim([str(i) for i in range(n_genes)], id = "gene")
+knots = la.Dim(range(n_knots), id = "knot")
+
+steps = np.random.choice([-1, 1], (n_genes, n_knots)) * np.random.normal(3., 1., (n_genes, n_knots)) * (np.random.random((n_genes, n_knots)) > 0.5)
+a_value = steps.cumsum(1)
+a_value = a_value - a_value.mean(1, keepdims = True)
+a = la.Fixed(
+    pd.DataFrame(
+        a_value,
+        columns = knots.index, index = genes.index),
+    label = "a"
+)
+intercept = la.Fixed(pd.Series(np.random.choice([-1, 1], n_genes) * np.random.normal(3., 1., n_genes) * (np.random.random(n_genes) > 0.5), index = genes.index), label = "intercept")
+scale = la.Fixed(pd.Series(np.random.uniform(1., 1.2, n_genes), index = genes.index), label = "scale")
+
+
+# %%
+y = la.links.scalar.Spline(x = x, a = a, b = intercept)
+dist = la.distributions.Normal(loc = y, scale = scale)
+
+
+# %%
+model_gs = la.Model(dist, label = "ground truth", symbol = "gs")
+model_gs.plot()
+
+# %%
+posterior = la.posterior.Posterior(dist)
+posterior.sample(1)
+
+
+# %%
+loc_value = posterior.samples[dist.loc].sel(sample = 0).to_pandas()
+observation_value = posterior.samples[dist].sel(sample = 0).to_pandas()
+fig, (ax0, ax1) = plt.subplots(1, 2, figsize = (10, 5))
+cell_order = model_gs.find_recursive("x").prior_pd().sort_values().index
+sns.heatmap(observation_value.loc[cell_order], ax = ax0)
+
+# %%
+observation_value = posterior.samples[dist].sel(sample = 0).to_pandas()
+gene_ids = genes.coords[:10]
+fig, axes = la.plotting.axes_wrap(len(gene_ids))
+cell_order = model_gs.find_recursive("x").prior_pd().sort_values().index
+x_value = model_gs.find_recursive("x").prior_pd()
+
+for gene_id, ax in zip(gene_ids, axes):
+    ax.scatter(
+        x_value.loc[cell_order],
+        observation_value.loc[cell_order, gene_id]
+    )
+    ax.plot(
+        x_value.loc[cell_order],
+        loc_value.loc[cell_order, gene_id]
+    )
+# sns.heatmap(observation_value.loc[cell_order], ax = ax0)
+
+# %% [markdown]
+# ## Regression with variational inference
+
+# %%
+s = la.Parameter(1., definition = scale, transforms = la.distributions.Exponential().biject_to())
+
+z = la.links.scalar.Spline(x, knot = model_gs.find_recursive("knot"), b = intercept, output = y.value_definition)
+
+dist = la.distributions.Normal(loc = z, scale = s)
+
+observation = la.Observation(observation_value, dist, label = "observation")
+
+# %%
+model = la.Model(observation)
+model.plot()
+
+
+# %%
+inference = la.infer.svi.SVI(model, [la.infer.loss.ELBO()], la.infer.optim.Adam(lr = 0.05))
+trainer = la.infer.trainer.Trainer(inference)
+trace = trainer.train(3000)
+trace.plot();
+# inference = la.infer.svi.SVI(model, [la.infer.loss.ELBO()], la.infer.optim.Adam(lr = 0.05))
+# trainer = la.infer.trainer.Trainer(inference)
+# trace = trainer.train(3000)
+# trace.plot();
+
+
+# %%
+z.a.value_definition
+
+# %%
+z.a.value_definition
+
+# %%
+z.reset_recursive()
+
+# %%
+z.run_recursive()
+
+# %%
+z.a.value_definition
+
+# %%
+observation.p.loc.a.q
+
+# %%
+observation.p.loc.a.q.value_definition
+
+# %%
+observed = la.posterior.Posterior(observation)
+observed.sample(10, subsample_n = 3)
+
+
+# %%
+fig, (ax0, ax1) = plt.subplots(1, 2, figsize = (10, 5))
+cell_order = model_gs.find_recursive("x").prior_pd().sort_values().index
+sns.heatmap(observation_value.loc[cell_order], ax = ax0)
+modelled_value = observed.samples[observation.p].sel(sample = 0).to_pandas()
+sns.heatmap(modelled_value.loc[cell_order], ax = ax1)
+
+
+# %%
+parameter_values = la.qa.cookbooks.check_parameters(la.qa.cookbooks.gather_parameters(["a", "intercept"], model_gs, observed))
+
+# %%
---
+++ cookbook/regression/splines_polation.py
@@ -0,0 +1,297 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.11.4
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# # Spline inter and extrapolation
+
+# %%
+from IPython import get_ipython
+if get_ipython():
+    get_ipython().run_line_magic('load_ext', 'autoreload')
+    get_ipython().run_line_magic('autoreload', '2')
+
+import numpy as np
+import pandas as pd
+import torch
+
+import xarray as xr
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import collections
+import math
+
+import latenta as la
+la.logger.setLevel("INFO")
+
+# %% [markdown]
+# ## Circular spline
+
+# %%
+n_cells = 2000
+cell_ids = [str(i) for i in range(n_cells)]
+cell_index = pd.Series(cell_ids, name = "cell")
+
+angle_value = pd.Series(np.linspace(2., 4., n_cells) % (2 * math.pi), index = cell_index)
+# angle_value = pd.Series(np.linspace(3., 5., n_cells) % (2 * math.pi), index = cell_index)
+# angle_value = pd.Series(np.linspace(-1., 1., n_cells) % (2 * math.pi), index = cell_index)
+angle = la.Fixed(angle_value, distribution = la.distributions.CircularUniform(), label = "angle", symbol = r"\theta")
+angle
+
+# %% [markdown]
+# ### Toy data
+
+# %%
+n_genes = 100
+
+observation_value = pd.DataFrame(
+    np.vstack([
+        np.random.normal(1., 0.5, n_cells),
+        np.sin(angle_value) + np.random.normal(0., 0.5, n_cells),
+        np.where((angle_value > 3), 1, 0) + np.random.normal(0., 0.5, n_cells),
+        np.exp(-(angle_value - 4)**2) + np.random.normal(0., 0.2, n_cells),
+        (angle_value > 2.5) * 1 + (angle_value > 3) * 1 - (angle_value > 3.5) * 3 + np.random.normal(0., 0.5, n_cells),
+    ]).T,
+    index = cell_index
+)
+observation_value.columns = [str(i) for i in range(observation_value.shape[1])]
+observation_value.columns.name = "gene"
+
+# %%
+fig, axes = la.plotting.axes_wrap(observation_value.shape[1])
+for gene_id, ax in zip(observation_value.columns, axes):
+    sns.scatterplot(x = angle_value, y = observation_value[gene_id], ax = ax)
+    ax.set_title(gene_id)
+
+# %% [markdown]
+# ### Modelling
+
+# %%
+x = angle
+x = la.Fixed((x.prior_xr() + 4.) % math.tau, distribution = x.distribution)
+
+# %%
+output_definition = la.Definition.from_xr(observation_value)
+output_definition
+
+# %%
+models = {}
+
+# %%
+z = la.links.scalar.CircularSpline(
+    x, 
+    output = output_definition,
+    n_knots = 10,
+    basis = la.links.scalar.spline.gaussian,
+    step_distribution = la.distributions.Normal(scale = 1.)
+)
+z.empirical = xr.DataArray(observation_value)
+s = la.Parameter(0.5)
+dist2 = la.distributions.Normal(loc = z, scale = s)
+observation = la.Observation(observation_value, dist2, label = "observation")
+
+models["random_walk"] = la.Model(observation)
+
+# %%
+observation.plot()
+
+# %%
+# retrain = False
+retrain = True
+for model in models.values():
+    if ("trace" not in model) or retrain:
+        inference = la.infer.svi.SVI(model, [la.infer.loss.ELBO()], la.infer.optim.Adam(lr = 0.05), 
+#                                      subsamplers = {"cell":la.infer.subsampling.Subsampler(150)}
+                                    )
+        trainer = la.infer.trainer.Trainer(inference)
+        trace = trainer.train(10000)
+        
+        for i in range(4):
+            inference = la.infer.svi.SVI(model, [la.infer.loss.ELBO()], la.infer.optim.Adam(lr = 0.01))
+            trainer = la.infer.trainer.Trainer(inference)
+            model["trace"] = trainer.train(10000)
+
+# %%
+# retrain = False
+for model in models.values():
+    if ("observed" not in model) or retrain:
+        model["observed"] = la.posterior.Observed(model.observation)
+        model["observed"].sample(100)
+        
+        if "causal" in model:
+            del model["causal"]
+
+# %%
+for model in models.values():
+#     del model["causal"]
+    if ("causal" not in model) or retrain:
+        causal = la.posterior.scalar.ScalarVectorCausal(x, model.observation)
+        causal.observed.sample(1)
+        causal.sample(100)
+        causal.sample_bootstrap(10)
+
+        model["causal"] = causal
+
+# %%
+feature_ids = observation_value.columns
+for model_id, model in models.items():
+    fig = model["causal"].plot_features(feature_ids = feature_ids);
+    for feature_id, ax in zip(feature_ids, fig.axes):
+        k = model["observed"].samples[model.observation.p.loc.knot].median("sample")
+        a = model["observed"].samples[model.observation.p.loc.a].median("sample").sel(gene = feature_id)
+        ax.scatter(k, a)
+
+#         baseline = model["observed"].samples[model.observation.p.loc.b].mean("sample").sel(gene = feature_id)
+#         ax.axhline(
+#             baseline,
+#             color = "#333333",
+#             dashes = [2]
+#         )
+#         ax.axhline(
+#             0,
+#             color = "#333333"
+#         )
+    fig.suptitle(model_id)
+    fig.tight_layout()
+
+# %% [markdown]
+# ## Regular spline
+
+# %%
+n_cells = 2000
+cell_ids = [str(i) for i in range(n_cells)]
+cell_index = pd.Series(cell_ids, name = "cell")
+
+x_value = pd.Series(np.hstack([np.linspace(0., 2., int(n_cells/2)), np.linspace(4., 6., int(n_cells/2))]), index = cell_index)
+x = la.Fixed(x_value, distribution = la.distributions.Uniform(0., 8.), label = "x", symbol = r"a")
+x
+
+# %% [markdown]
+# ### Toy data
+
+# %%
+observation_value = pd.DataFrame(
+    np.vstack([
+        np.random.normal(1., 0.5, n_cells),
+        x_value * 2 + np.random.normal(0., 0.5, n_cells),
+        np.where((x_value > 3), 1, 0) + np.random.normal(0., 0.5, n_cells),
+        np.exp(x_value / 6) + np.random.normal(0., 0.2, n_cells),
+        ((x_value - 2) **2) + np.random.normal(0., 0.5, n_cells),
+        (-(2 * (x_value - 3)) **2) / 20 + np.random.normal(0., 0.5, n_cells),
+    ]).T,
+    index = cell_index
+)
+observation_value.columns = [str(i) for i in range(observation_value.shape[1])]
+observation_value.columns.name = "gene"
+
+# %%
+fig, axes = la.plotting.axes_wrap(observation_value.shape[1])
+for gene_id, ax in zip(observation_value.columns, axes):
+    sns.scatterplot(x = x_value, y = observation_value[gene_id], ax = ax)
+
+# %% [markdown]
+# ### Modelling
+
+# %%
+output_definition = la.Definition.from_xr(observation_value)
+output_definition
+
+# %%
+models = {}
+
+# %%
+# output_distribution = la.distributions.Normal(scale = la.Fixed(observation_value.std() * 2), definition = output_definition[-1:])
+z = la.links.scalar.Spline(
+    x, 
+    output = output_definition,
+    n_knots = 20,
+    step_distribution = la.distributions.Normal(scale = 1.)
+#     output_distribution = output_distribution
+)
+z.empirical = xr.DataArray(observation_value)
+s = la.Parameter(0.5)
+dist2 = la.distributions.Normal(loc = z, scale = s)
+observation = la.Observation(observation_value, dist2, label = "observation")
+
+models["random_walk"] = la.Model(observation)
+
+# %%
+observation.plot()
+
+# %%
+# retrain = False
+retrain = True
+for model in models.values():
+    if ("trace" not in model) or retrain:
+        inference = la.infer.svi.SVI(model, [la.infer.loss.ELBO()], la.infer.optim.Adam(lr = 0.05), 
+#                                      subsamplers = {"cell":la.infer.subsampling.Subsampler(150)}
+                                    )
+        trainer = la.infer.trainer.Trainer(inference)
+        trace = trainer.train(10000)
+        
+        for i in range(5):
+            inference = la.infer.svi.SVI(model, [la.infer.loss.ELBO()], la.infer.optim.Adam(lr = 0.01))
+            trainer = la.infer.trainer.Trainer(inference)
+            model["trace"] = trainer.train(10000)
+
+# %%
+# retrain = False
+for model in models.values():
+    if ("observed" not in model) or retrain:
+        model["observed"] = la.posterior.Observed(model.observation)
+        model["observed"].sample(100)
+        
+        if "causal" in model:
+            del model["causal"]
+
+# %%
+for model in models.values():
+#     del model["causal"]
+    if ("causal" not in model) or retrain:
+        causal = la.posterior.scalar.ScalarVectorCausal(x, model.observation, observed = model["observed"])
+        causal.sample(100)
+        causal.sample_bootstrap(10)
+
+        model["causal"] = causal
+
+# %%
+feature_ids = observation_value.columns
+for model_id, model in models.items():
+    fig = model["causal"].plot_features(feature_ids = feature_ids);
+    for feature_id, ax in zip(feature_ids, fig.axes):
+        k = model["observed"].samples[model.observation.p.loc.knot].mean("sample")
+        a = model["observed"].samples[model.observation.p.loc.a].mean("sample").sel(gene = feature_id)
+        ax.scatter(k, a)
+
+#         baseline = model["observed"].samples[model.observation.p.loc.b].mean("sample").sel(gene = feature_id)
+#         ax.axhline(
+#             baseline,
+#             color = "#333333",
+#             dashes = [2]
+#         )
+#         ax.axhline(
+#             0,
+#             color = "#333333"
+#         )
+    fig.suptitle(model_id)
+    fig.tight_layout()
+
+# %% [markdown]
+# Conclusion:
+# - Splines are useful for interpolation
+# - Splines are not useful for extrapolation, and we need to be careful when training
+
+# %%

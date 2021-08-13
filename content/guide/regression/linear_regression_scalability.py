01439764aeab33f1c77da2d25da3a14e1d4f10c5
Wouter Saelens <wouter.saelens@gmail.com>
2021-08-13 21:59:44 +0200

    remove empirical from regression guide

--- content/guide/regression/linear_regression_scalability.py
+++ content/guide/regression/linear_regression_scalability.py
@@ -93,10 +93,6 @@ observed.sample(10, subsample_n = 1)
 
 
 # %%
-empirical = la.posterior.Empirical(observation)
-empirical.sample(observation["cell"].coords[:1000])
-
-# %%
 fig, (ax0, ax1) = plt.subplots(1, 2, figsize = (10, 5))
 cell_order = model_gs.find("x").prior_pd().sort_values().index
 sns.heatmap(observation_value.loc[cell_order], ax = ax0)
@@ -109,152 +105,9 @@ parameter_values = la.qa.cookbooks.check_parameters(la.qa.cookbooks.gather_param
 
 # %%
 causal = la.posterior.scalar.ScalarVectorCausal(x, observation, observed = observed)
-causal.sample(10)
+causal.sample(10, samples = la.posterior.FileSamples("./test6.nc"))
 causal.sample_random(10)
-causal.sample_empirical()
+causal.sample_empirical(samples = la.posterior.FileSamples("./test7.nc"))
 causal.plot_features(observation.p.loc);
-
-# %% [markdown]
-# ## Linear regression with maximal likelihood and inferred x
-
-# %%
-x = la.Parameter(0.5, definition = x, transforms = la.distributions.Uniform(0., 1.).biject_to(), label = "x")
-
-# %%
-a = la.Parameter(0., definition = slope, transforms = la.distributions.Normal(scale = 1.).biject_to())
-b = la.Parameter(0., definition = intercept, transforms = la.distributions.Normal(scale = 1.).biject_to())
-s = la.Parameter(1., definition = scale, transforms = la.distributions.Exponential().biject_to())
-
-z = la.links.scalar.Linear(x, a, b)
-
-dist = la.distributions.Normal(loc = z, scale = s)
-
-observation = la.Observation(observation_value, dist, label = "observation")
-
-# %%
-model = la.Model(observation)
-model.plot()
-
-
-# %%
-inference = la.infer.svi.SVI(model, [la.infer.loss.ELBO()], la.infer.optim.Adam(lr = 0.05))
-trainer = la.infer.trainer.Trainer(inference)
-trace = trainer.train(3000)
-trace.plot();
-
-
-# %%
-observed = la.posterior.Posterior(observation)
-observed.sample(10, subsample_n = 3)
-
-
-# %%
-fig, (ax0, ax1) = plt.subplots(1, 2, figsize = (10, 5))
-cell_order = model_gs.find("x").prior_pd().sort_values().index
-sns.heatmap(observation_value.loc[cell_order], ax = ax0)
-modelled_value = observed.samples[observation.p].sel(sample = 0).to_pandas()
-sns.heatmap(modelled_value.loc[cell_order], ax = ax1)
-
-
-# %%
-parameter_values = la.qa.cookbooks.check_parameters(la.qa.cookbooks.gather_parameters(["intercept", "slope", "x"], model_gs, observed))
-
-# %% [markdown]
-# ## Linear regression with variational inference and latent x
-
-# %%
-x = la.Latent(la.distributions.Uniform(0., 3.), definition = la.Definition([cells]), label = "x")
-
-z = la.links.scalar.Linear(x, output = la.Definition([cells, genes]), a = True, b = True)
-
-dist = la.distributions.Normal(loc = z, scale = s)
-
-observation = la.Observation(observation_value, dist, label = "observation")
-
-# %%
-model = la.Model(observation)
-model.plot()
-
-
-# %%
-inference = la.infer.svi.SVI(model, [la.infer.loss.ELBO()], la.infer.optim.Adam(lr = 0.05))
-trainer = la.infer.trainer.Trainer(inference)
-trace = trainer.train(3000)
-trace.plot();
-
-
-# %%
-observation.reset()
-observation.run()
-observation.p.loc.x.likelihood.sum()
-
-# %%
-observed = la.posterior.Posterior(observation)
-observed.sample(10, subsample_n = 4)
-
-
-# %%
-fig, (ax0, ax1) = plt.subplots(1, 2, figsize = (10, 5))
-cell_order = model_gs.find("x").prior_pd().sort_values().index
-sns.heatmap(observation_value.loc[cell_order], ax = ax0)
-modelled_value = observed.samples[observation.p].sel(sample = 0).to_pandas()
-sns.heatmap(modelled_value.loc[cell_order], ax = ax1)
-
-
-# %%
-parameter_values = la.qa.cookbooks.check_parameters(la.qa.cookbooks.gather_parameters(["intercept", "slope", "x"], model_gs, observed))
-
-# %%
-z.empirical = xr.DataArray(observation_value)
-x.distribution = la.distributions.Uniform(0., 3.)
-causal = la.posterior.scalar.ScalarVectorCausal(x, observation, observed = observed)
-causal.sample(10)
-causal.sample_random(10)
-causal.plot_features(observation.p.loc);
-
-# %% [markdown]
-# ## Linear regression with variational inference and amortized latent x
-
-# %%
-amortization_input = la.Fixed(observation_value, label = "observed")
-nn = la.amortization.Encoder(amortization_input, x)
-
-
-# %%
-model = la.Model(observation)
-model.plot()
-
-
-# %%
-inference = la.infer.svi.SVI(model, [la.infer.loss.ELBO()], la.infer.optim.Adam(lr = 0.05))
-trainer = la.infer.trainer.Trainer(inference)
-trace = trainer.train(3000)
-trace.plot();
-
-
-# %%
-observed = la.posterior.Posterior(observation)
-observed.sample(10, subsample_n = 3)
-
-
-# %%
-parameter_values = la.qa.cookbooks.check_parameters(la.qa.cookbooks.gather_parameters(["intercept", "slope", "x"], model_gs, observed))
-
-# %%
-fig, (ax0, ax1) = plt.subplots(1, 2, figsize = (10, 5))
-cell_order = model_gs.find("x").prior_pd().sort_values().index
-sns.heatmap(observation_value.loc[cell_order], ax = ax0)
-modelled_value = observed.samples[observation.p].sel(sample = 0).to_pandas()
-sns.heatmap(modelled_value.loc[cell_order], ax = ax1)
-
-# %%
-z.empirical = xr.DataArray(observation_value)
-x.distribution = la.distributions.Uniform(0., 3.)
-causal = la.posterior.scalar.ScalarVectorCausal(x, observation, observed = observed)
-causal.sample(10)
-causal.sample_random(10)
-causal.plot_features();
-
-# %%
 
 # %%

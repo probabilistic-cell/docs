5fecae78d6128717a9ca24783b6d7be03f4f2a25
Wouter Saelens <wouter.saelens@gmail.com>
2021-10-02 17:07:29 +0200

    update leaf variables

--- content/guide/variables/leaves.py
+++ content/guide/variables/leaves.py
@@ -18,9 +18,10 @@
 
 # %%
 from IPython import get_ipython
+
 if get_ipython():
-    get_ipython().run_line_magic('load_ext', 'autoreload')
-    get_ipython().run_line_magic('autoreload', '2')
+    get_ipython().run_line_magic("load_ext", "autoreload")
+    get_ipython().run_line_magic("autoreload", "2")
 
 import latenta as la
 import scanpy as sc
@@ -32,15 +33,26 @@ import torch
 
 # %%
 cells = la.Dim(10000, "cell")
-genes = la.Dim(10000, "gene")
+genes = la.Dim(1000, "gene")
+definition = la.Definition([cells, genes])
+
+# %%
+import tempfile
+
+# %%
+tmpdir = tempfile.TemporaryDirectory().name
+pathlib.Path(tmpdir).mkdir()
+
+# %%
+filesize
 
 # %% [markdown]
 # ## Loaders
 
 # %%
-value = np.zeros((len(cells), len(genes)))
+value = np.zeros(definition.shape)
 for i in range(value.shape[0]):
-    value[i, np.random.choice(value.shape[1])] = 1.
+    value[i, np.random.choice(value.shape[1])] = 1.0
 
 # %%
 value.sum()
@@ -49,8 +61,8 @@ value.sum()
 # ### Memory loader
 
 # %%
-loader = la.variables.loaders.MemoryLoader(value)
-fixed = la.Fixed(loader, definition = la.Definition([cells, genes]))
+loader = la.loaders.MemoryLoader(value)
+fixed = la.Fixed(loader, definition=la.Definition([cells, genes]))
 
 # %% [markdown]
 # ### Sparse loader
@@ -65,10 +77,11 @@ from gc import get_referents
 # Exclude modules as well.
 BLACKLIST = type, ModuleType, FunctionType
 
+
 def getsize(obj):
     """sum size of object & members."""
     if isinstance(obj, BLACKLIST):
-        raise TypeError('getsize() does not take argument of type: '+ str(type(obj)))
+        raise TypeError("getsize() does not take argument of type: " + str(type(obj)))
     seen_ids = set()
     size = 0
     objects = [obj]
@@ -89,64 +102,147 @@ def getsize(obj):
 # Obviously, a large difference in memory usage.
 
 # %%
-getsize(loader)/1024/1024
+getsize(loader) / 1024**2
 
 # %% [markdown]
 # Using 3GB (or more) on current GPUs would be prohibitive.
 
 # %%
-loader_sparse = la.variables.loaders.SparseDenseMemoryLoader(la.sparse.COO.from_numpy_array(value))
-fixed_sparse = la.Fixed(loader_sparse, definition = la.Definition([cells, genes]))
+sparse = la.sparse.COOMatrix.from_numpy_array(value)
+loader_sparse = la.loaders.SparseMemoryLoader(
+    sparse
+)
+fixed_sparse = la.Fixed(loader_sparse, definition=la.Definition([cells, genes]))
 
 # %%
-getsize(loader_sparse)/1024/1024
+loader_sparse.initialize(fixed_sparse)
+
+# %%
+getsize(loader_sparse) / 1024**2
 
 # %% [markdown]
 # #### Initialization
 
 # %% [markdown]
-# There are several ways to initialize the COO:
+# There are several ways to initialize the COOMatrix:
 
 # %%
-la.sparse.COO.from_numpy_array(value)
+la.sparse.COOMatrix.from_numpy_array(value)
 
 # %%
 value_scipy_coo = scipy.sparse.coo_matrix(value)
-la.sparse.COO.from_scipy_coo(value_scipy_coo)
+la.sparse.COOMatrix.from_scipy_coo(value_scipy_coo)
 
 # %%
 value_scipy_csr = scipy.sparse.csr_matrix(value)
-la.sparse.COO.from_scipy_csr(value_scipy_csr)
+la.sparse.COOMatrix.from_scipy_csr(value_scipy_csr)
 
 # %% [markdown]
 # #### Timing
 
 # %% [markdown]
-# When data is accessed from a SparseDenseMemoryLoader, it is converted to a dense tensor. This can cause a slowdown:
+# When data is accessed from a SparseMemoryLoader, it is converted to a dense tensor. This can cause a slowdown:
 
 # %%
 import timeit
 
 # %%
-time = timeit.timeit("loader.get()", number = 1, globals = globals())
+time = timeit.timeit("loader.get()", number=1, globals=globals())
 time
 
 # %%
-time = timeit.timeit("loader_sparse.get()", number = 1, globals = globals())
+time = timeit.timeit("loader_sparse.get()", number=1, globals=globals())
 time
 
 # %% [markdown]
-# Speed is however more competitive when taking only a subsample:
+# Speed is however much more competitive when taking only a subsample, which is the typical use case anyway when working with large sparse tensors:
 
 # %%
-idx = {"cell":torch.tensor(range(500))}
+idx = {"cell": torch.tensor(range(500))}
 
 # %%
-time = timeit.timeit("loader.get(idx)", number = 1, globals = globals())
+time = timeit.timeit("loader.get(idx)", number=1, globals=globals())
 time
 
 # %%
-time = timeit.timeit("loader_sparse.get(idx)", number = 1, globals = globals())
+time = timeit.timeit("loader_sparse.get(idx)", number=1, globals=globals())
 time
+
+# %% [markdown]
+# ### File loader
+
+# %% [markdown]
+# Supply a path to a pickle. The value will not be saved along with the loader
+
+# %%
+import pickle
+import tempfile
+import pathlib
+import os
+
+# %%
+path = pathlib.Path(tempfile.NamedTemporaryFile().name)
+pickle.dump(value, path.open("wb"))
+
+# %%
+filesize = os.path.getsize(path) / 1024**2
+print(filesize, "Mb")
+
+# %%
+assert filesize > 1024
+
+# %%
+fixed = la.Fixed(path, definition = la.Definition([cells, genes]))
+
+# %%
+fixed_path = pathlib.Path(tempfile.NamedTemporaryFile().name)
+pickle.dump(fixed, fixed_path.open("wb"))
+
+# %%
+filesize = os.path.getsize(fixed_path) / 1024**2
+print(filesize, "Mb")
+
+# %%
+assert filesize < 3
+
+# %% [markdown]
+# ### Sparse File Loader
+
+# %% [markdown]
+# Similarly, we can load a sparse matrix from a file. This is especially useful with larger tensors, i.e. over a billion values (e.g. hundred thousands cells and ten thousand genes)
+
+# %%
+path = pathlib.Path(tempfile.NamedTemporaryFile().name)
+pickle.dump(sparse, path.open("wb"))
+
+# %%
+filesize = os.path.getsize(path) / 1024**2
+print(filesize, "Mb")
+
+# %%
+assert filesize > 3
+
+# %%
+fixed = la.Fixed(
+    la.loaders.COOMatrixFileLoader(path),
+    definition = la.Definition([cells, genes])
+)
+
+# %%
+fixed_path = pathlib.Path(tempfile.NamedTemporaryFile().name)
+pickle.dump(fixed, fixed_path.open("wb"))
+
+# %%
+filesize = os.path.getsize(fixed_path) / 1024**2
+print(filesize, "Mb")
+
+# %%
+assert filesize < 3
+
+# %%
+fixed2 = pickle.load(fixed_path.open("rb"))
+
+# %%
+fixed2.loader.get()
 
 # %%
